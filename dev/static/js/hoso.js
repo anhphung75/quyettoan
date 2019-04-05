ca2516a764dbde57217889dc2dc29723c8adcc78
anhphung75 <anhphung75@gmail.com>
2019-04-05 17:03:42 +0700

    20190405 1703

--- dev/static/js/hoso.js
+++ dev/static/js/hoso.js
@@ -1,101 +1,183 @@
 /* import * as hoso from "./hoso/api";
 var TatcaHoso = hoso.Tat(curYear);
 */
-var listJson= [
-    {mahoso:'GM01', sohoso:'GM12345/10', khachhang:'cty a', diachi:'123 duong 1, quan 2'},
-    { mahoso: 'GM02', sohoso: 'GM12345/11', khachhang: 'cty b', diachi: '123 duong 1, quan 3' },
-    { mahoso: 'GM03', sohoso: 'GM12345/12', khachhang: 'cty c', diachi: '123 duong 1, quan 4' },
-    { mahoso: 'GM04', sohoso: 'GM12345/13', khachhang: 'cty d', diachi: '123 duong 1, quan 5' },
-    { mahoso: 'GM05', sohoso: 'GM12345/14', khachhang: 'cty e', diachi: '123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6' },
-    { mahoso: 'GM06', sohoso: 'GM12345/15', khachhang: 'cty f', diachi: '123 duong 1, quan 7' },
-    { mahoso: 'GM07', sohoso: 'GM12345/16', khachhang: 'cty g', diachi: '123 duong 1, quan 8' },
-    { mahoso: 'GM08', sohoso: 'GM12345/17', khachhang: 'cty h', diachi: '123 duong 1, quan 9' },
-    { mahoso: 'GM09', sohoso: 'GM12345/18', khachhang: 'cty i', diachi: '123 duong 1, quan 10' },
-    { mahoso: 'GM10', sohoso: 'GM12345/19', khachhang: 'cty j', diachi: '123 duong 1, quan 12' }
+var listJson = [
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM01",
+        sohoso: "GM12345/10",
+        khachhang: "cty a",
+        diachi: "123 duong 1, quan 2"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM02",
+        sohoso: "GM12345/11",
+        khachhang: "cty b",
+        diachi: "123 duong 1, quan 3"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM03",
+        sohoso: "GM12345/12",
+        khachhang: "cty c",
+        diachi: "123 duong 1, quan 4"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM04",
+        sohoso: "GM12345/13",
+        khachhang: "cty d",
+        diachi: "123 duong 1, quan 5"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM05",
+        sohoso: "GM12345/14",
+        khachhang: "cty e",
+        diachi:
+            "123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 ,123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6 123 duong 1, quan 6"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM06",
+        sohoso: "GM12345/15",
+        khachhang: "cty f",
+        diachi: "123 duong 1, quan 7"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM07",
+        sohoso: "GM12345/16",
+        khachhang: "cty g",
+        diachi: "123 duong 1, quan 8"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM08",
+        sohoso: "GM12345/17",
+        khachhang: "cty h",
+        diachi: "123 duong 1, quan 9"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM09",
+        sohoso: "GM12345/18",
+        khachhang: "cty i",
+        diachi: "123 duong 1, quan 10"
+    },
+    {
+        dvtc: "QLMLQ2",
+        sodot: "GM01",
+        mahoso: "GM10",
+        sohoso: "GM12345/19",
+        khachhang: "cty j",
+        diachi: "123 duong 1, quan 12"
+    }
 ];
 
 var webapp = new Vue({
-    el: '#webapp',
+    el: "#webapp",
     delimiters: ["{`", "`}"],
     data() {
         return {
-        curYear: new Date().getFullYear(),
-        dsnam: [2017, 2018 , 2019 , 2020],
-        rows: listJson,
-        s_mahoso: '',
-        s_sohoso: '',
-        s_khachhang: '',
-        s_diachi: '',
-        /* pagination */
-        curPage: 0,
-        itemsPerPage:7,
-        filters: {},
-        sortBy: {
-            colName: 'stt',
-            direction: 1
-        }
-    }},
+            curYear: new Date().getFullYear(),
+            dsnam: [2017, 2018, 2019, 2020],
+            recs: listJson,
+            s_dvtc: "",
+            s_sodot: "",
+            s_sohoso: "",
+            s_khachhang: "",
+            s_diachi: "",
+            /* pagination */
+            curPage: 0,
+            itemsPerPage: 10,
+            sortBy: {
+                colName: "stt",
+                direction: 1
+            }
+        };
+    },
     computed: {
-        rowsFilter() {
-            let rowX= this.rows;
+        recsFilter() {
+            let recs = this.recs;
+            if (this.s_dvtc.length > 0) {
+                recs = recs.filter(
+                    item =>
+                        item.dvtc
+                            .toLowerCase()
+                            .indexOf(this.s_dvtc.toLowerCase()) > -1
+                );
+            }
+            if (this.s_sodot.length > 0) {
+                recs = recs.filter(
+                    item =>
+                        item.sodot
+                            .toLowerCase()
+                            .indexOf(this.s_sodot.toLowerCase()) > -1
+                );
+            }
             if (this.s_sohoso.length > 0) {
-                rowX = rowX.filter(item =>
-                    (item.sohoso.toLowerCase().indexOf(this.s_sohoso.toLowerCase()) > -1));
-            };
+                recs = recs.filter(
+                    item =>
+                        item.sohoso
+                            .toLowerCase()
+                            .indexOf(this.s_sohoso.toLowerCase()) > -1
+                );
+            }
             if (this.s_khachhang.length > 0) {
-                rowX = rowX.filter(item =>
-                    (item.khachhang.toLowerCase().indexOf(this.s_khachhang.toLowerCase()) > -1));
-            };
+                recs = recs.filter(
+                    item =>
+                        item.khachhang
+                            .toLowerCase()
+                            .indexOf(this.s_khachhang.toLowerCase()) > -1
+                );
+            }
             if (this.s_diachi.length > 0) {
-                rowX = rowX.filter(item =>
-                    (item.diachi.toLowerCase().indexOf(this.s_diachi.toLowerCase()) > -1));
-            };
-            return rowX;
+                recs = recs.filter(
+                    item =>
+                        item.diachi
+                            .toLowerCase()
+                            .indexOf(this.s_diachi.toLowerCase()) > -1
+                );
+            }
+            return recs;
         },
-        showing() {
-            let curPage = this.curPage + 0;
-            let begin = (curPage * this.itemsPerPage) + 1;
-            let end = begin - 1 + this.rowsFilter.length;
-            let total = this.rowsFilter.length;
-            return {
-                begin, end, total
-            };
+        recsTotal() {
+            return this.recsFilter.length;
         },
-        totalPages() {
-            return Math.ceil(this.rowsFilter.length / this.itemsPerPage);
+        pageTotal() {
+            return Math.ceil(this.recsFilter.length / this.itemsPerPage);
         },
-        rowsPage() {
-            if (this.curPage != 0 && this.curPage >= this.totalPages) {
-                this.curPage = this.totalPages - 1
+        recsPage() {
+            if (this.curPage != 0 && this.curPage >= this.pageTotal) {
+                this.curPage = this.pageTotal - 1;
             }
             let index = this.curPage * this.itemsPerPage;
-            let rows = this.rowsFilter;
+            let recs = this.recsFilter;
             let colName = this.sortBy.colName;
-
-            rows = rows.sort((a, b) => {
-
+            recs = recs.sort((a, b) => {
                 if (this.sortBy.direction == 1)
-                    return a[this.sortBy.colName] >
-                        b[this.sortBy.colName]
-                        ? 1
-                        : -1;
-                else
-                    return a[this.sortBy.colName] <
-                        b[this.sortBy.colName]
-                        ? 1
-                        : -1;
-
+                    return a[colName] > b[colName] ? 1 : -1;
+                else return a[colName] < b[colName] ? 1 : -1;
                 return 0;
-
             });
-
-            return rows.slice(index, (index + this.itemsPerPage))
+            return recs.slice(index, index + this.itemsPerPage);
         },
         firstPage() {
             return 0;
         },
         lastPage() {
-            return this.totalPages - 1;
+            return this.pageTotal - 1;
         },
         nextPage() {
             let next = this.curPage + 1;
@@ -106,49 +188,34 @@ var webapp = new Vue({
             return prev < this.firstPage ? this.firstPage : prev;
         },
         pages() {
+            let a = this.curPage - 2;
+            if (a > this.pageTotal - 5) {
+                a = this.pageTotal - 5;
+            }
+            if (a < 0) {
+                a = 0;
+            }
+            let z = a + 5;
+            if (z > this.pageTotal) {
+                z = this.pageTotal;
+            }
             let pages = [];
-            for (let i = 0; i < this.totalPages; i++) {
+            for (let i = a; i < z; i++) {
                 pages.push(i);
             }
-            if (pages.length > 5) {
-                let active = this.curPage,
-                    before = 2,
-                    after = 2,
-                    last = pages.length - 1,
-                    diff;
-
-                if (active - before < 0) {
-                    diff = active - before;
-                    diff = 0 - diff;
-                    after += diff;
-                    before += -diff;
-                }
-                if (active + after > last) {
-                    diff = active + after;
-                    diff = last - diff;
-                    after += diff;
-                    before += -diff;
-                }
-                pages = pages.reduce((pre, cur) => {
-                    if (cur >= active - before && cur <= active + after) {
-                        pre.push(cur)
-                    }
-                    return pre;
-                }, [])
-            }
+            console.log("list page show =" + pages);
             return pages;
-        },
+        }
     },
     methods: {
         setPage(page) {
-            this.curPage = page
+            this.curPage = page;
         },
         sortByColumn(colName) {
             this.sortBy.colName == colName
-                ? (this.sortBy.direction =
-                      this.sortBy.direction * -1)
+                ? (this.sortBy.direction = this.sortBy.direction * -1)
                 : (this.sortBy.direction = 1);
             this.sortBy.colName = colName;
-        },
+        }
     }
 });
